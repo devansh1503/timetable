46af3c827352ff8baaa636f7c9d5d82297e1b3fd
Devansh <guyawsm@gmail.com>
2023-01-06 13:25:53 +0530

    dojgbaef oh

--- Controllers/time.control.js
+++ Controllers/time.control.js
@@ -5,7 +5,10 @@ function getthedata(){
     const currdate = new Date()
     console.log(currdate)
     const day = currdate.getDay()-1
-    const hrs = currdate.toLocaleString(undefined, {timeZone: 'Asia/Kolkata'}).split(',')[1].split(":")[0]
+    var hrs = currdate.toLocaleString(undefined, {timeZone: 'Asia/Kolkata'}).split(',')[1].split(":")[0]
+    var ampm = currdate.toLocaleString(undefined, {timeZone: 'Asia/Kolkata'}).split(',')[1].split(":")[2].split(" ")[1]
+    hrs = ampm=="PM"? +hrs+12 : +hrs
+    console.log(ampm)
     const tt = data[days[day]]
     var res = {
         "Subject":"Kitna Padega bsdk",
