849c4fd8ddff3f533d046f93f707c3db201630e9
Devansh <guyawsm@gmail.com>
2023-03-18 18:51:25 +0530

    dtgjaetjrjatyhsrtf

--- Controllers/mess.control.js
+++ Controllers/mess.control.js
@@ -6,13 +6,12 @@ function getmessdata(){
     day = day===-1?6:day
     var hrs = +currdate.toLocaleString(undefined, {timeZone: 'Asia/Kolkata'}).split(',')[1].split(":")[0]
     var ampm = currdate.toLocaleString(undefined, {timeZone: 'Asia/Kolkata'}).split(',')[1].split(":")[2].split(" ")[1]
-    console.log(hrs)
-    if(ampm==="PM" && hrs!=12){
+    if(ampm==="PM" && hrs!==12){
         console.log("condition achieved")
         hrs = hrs+12
     }
-    // hrs = (ampm=="PM" && hrs!=12)? +hrs+12 : +hrs
     console.log(hrs)
+    console.log(ampm)
     const tt = mdata[day]
     var res = {}
     tt.forEach(element => {
