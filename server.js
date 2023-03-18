6c1630b3aedada939644f5ad308bac10fa22e7b9
Devansh <guyawsm@gmail.com>
2023-03-18 18:32:33 +0530

    hvblvbbbjpobpuhbuigbui

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const { timeRoute } = require('./Router/timeroute')
 const cors = require('cors')
-const { AuthRoute } = require('./Router/authroute')
-const passport = require('passport')
-const { Strategy } = require('passport-google-oauth20')
 const cookieSession = require('cookie-session')
 
 const app = express()
@@ -11,25 +8,6 @@ const app = express()
 app.use(cors({}))
 app.use(express.json())
 app.set('view engine', 'ejs')
-
-// app.use(cookieSession({
-//     name:'login-session',
-//     maxAge: 5*60*1000,
-//     keys:["Secret","Secret rotation"]
-// }))
-// app.use(passport.initialize())
-// app.use(passport.session())
-
-// app.use(AuthRoute)
-
-// const checkLoggedIn = (req,res,next) =>{
-//     const lgin = req.isAuthenticated();
-//     if(!lgin){
-//         return res.status(401).json({"session-expired":true})
-//     }
-//     next()
-// }
-// app.use(checkLoggedIn)
 
 app.use(timeRoute)
 
