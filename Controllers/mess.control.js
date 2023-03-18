59ed049be8180195c82d07b405b45f5fd92a8bb3
Devansh <guyawsm@gmail.com>
2023-03-18 18:55:19 +0530

    vb zcv nvzdbn

--- Controllers/mess.control.js
+++ Controllers/mess.control.js
@@ -6,7 +6,7 @@ function getmessdata(){
     day = day===-1?6:day
     var hrs = +currdate.toLocaleString(undefined, {timeZone: 'Asia/Kolkata'}).split(',')[1].split(":")[0]
     var ampm = currdate.toLocaleString(undefined, {timeZone: 'Asia/Kolkata'}).split(',')[1].split(":")[2].split(" ")[1]
-    if(ampm==="PM" && hrs!==12){
+    if(ampm!=="AM" && hrs!==12){
         console.log("condition achieved")
         hrs = hrs+12
     }
